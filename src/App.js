fb11a467fc6ab18d27299c6363809a197449ddca
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-04-17 01:31:53 +0600

    added navbar and install router tailwind and icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import Header from './pages/SharedPage/Header/Header';
 
 function App() {
   return (
     <>
-      <h1>HOME</h1>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </main>
     </>
   );
 }
